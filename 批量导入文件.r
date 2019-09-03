0e3464801c4161afbeab95245dcdd8fbd0a3b11b
Boxiaogirl <54805995+Boxiaogirl@users.noreply.github.com>
2019-09-03 18:29:45 +0800

    Update 批量导入文件.r

--- 批量导入文件.r
+++ 批量导入文件.r
@@ -1,3 +1,6 @@
+#主要通过list.files函数获取文件名，用for循环进行读取，现在展示一段批量读取400个病人的基因测序文件，文件类型为txt文本。#
+
+
 library(dplyr)
 nameList <- list.files()
 matrix <- read.table(nameList[1],quote = "",sep = "\t",check.names = F,header = T)[c(1:2)]for (i in 2:length(nameList)){
